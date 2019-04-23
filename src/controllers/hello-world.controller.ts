411ddb1508d35aa521db6075bcf70c4df520b693
Sean Sundberg <seansund@us.ibm.com>
2019-04-22 23:48:08 -0500

    Adds hello-world.controller test for '/hello should return 200 status'

---
+++ src/controllers/hello-world.controller.ts
@@ -0,0 +1,10 @@
+import {GET, Path} from 'typescript-rest';
+
+export class HelloWorldController {
+
+  @Path('/hello')
+  @GET
+  async sayHelloToUnknownUser(): Promise<string> {
+    return '';
+  }
+}
--- test/controllers/hello-world.controller.test.ts
+++ test/controllers/hello-world.controller.test.ts
@@ -1,6 +1,23 @@
+import {Application} from 'express';
+import * as request from 'supertest';
+import {ApiServer} from '../../src/server';
+
 describe('Hello controller', () => {
+
+  let app: Application;
+
+  beforeAll(() => {
+    app = new ApiServer().getApp();
+  });
 
   test('canary validates test infrastructure', () => {
     expect(true).toBe(true);
   });
+
+  describe('Given /hello', () => {
+    test('should return 200 status', done => {
+      request(app).get('/hello').expect(200, done);
+    });
+  });
+
 });
