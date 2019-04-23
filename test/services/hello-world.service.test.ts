62a51f844afa9f1496e012dbd48935745efabf00
Sean Sundberg <seansund@us.ibm.com>
2019-04-23 09:44:24 -0500

    Adds hello-world.service test - 'Given greeting() when "Juan" provided then return "Hello, Juan!"' - failing

--- test/services/hello-world.service.test.ts
+++ test/services/hello-world.service.test.ts
@@ -1,6 +1,25 @@
+class HelloWorldService {
+  greeting(name: string) {
+  }
+}
 
 describe('Hello World service', () =>{
   test('canary test verifies test infrastructure', () => {
     expect(true).toBe(true);
   });
+
+  let service: HelloWorldService;
+  beforeAll(() => {
+    service = new HelloWorldService();
+  });
+
+  describe('Given greeting()', () => {
+    describe('when "Juan" provided', () => {
+      const name = 'Juan';
+      test('then return "Hello, Juan!"', () => {
+        expect(service.greeting(name)).toEqual(`Hello, ${name}!`);
+      });
+    });
+  });
+
 });
