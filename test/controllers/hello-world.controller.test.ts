7420e9f89dd870cf3235e226cd105e04bad2d5e5
Sean Sundberg <seansund@us.ibm.com>
2019-04-23 11:20:26 -0500

    Adds hello-world.service test - refactors controller test to inject mock service

--- test/controllers/hello-world.controller.test.ts
+++ test/controllers/hello-world.controller.test.ts
@@ -1,13 +1,28 @@
 import {Application} from 'express';
 import * as request from 'supertest';
 import {ApiServer} from '../../src/server';
+import {HelloWorldService} from '../../src/services';
+import {Scope} from 'typescript-ioc';
+
+class MockHelloWorldService implements HelloWorldService {
+  greeting = jest.fn().mockName('greeting');
+}
 
 describe('Hello controller', () => {
 
+  let apiServer: ApiServer;
   let app: Application;
+  let mockGreeting: jest.Mock;
 
-  beforeAll(() => {
-    app = new ApiServer().getApp();
+  beforeEach(() => {
+    apiServer = new ApiServer();
+
+    app = apiServer.getApp();
+
+    apiServer.bind(HelloWorldService).scope(Scope.Singleton).to(MockHelloWorldService);
+
+    const mockService: HelloWorldService = apiServer.get(HelloWorldService);
+    mockGreeting = mockService.greeting as jest.Mock;
   });
 
   test('canary validates test infrastructure', () => {
@@ -15,24 +30,34 @@ describe('Hello controller', () => {
   });
 
   describe('Given /hello', () => {
+    const expectedResponse = 'Hello there!';
+
+    beforeEach(() => {
+      mockGreeting.mockReturnValueOnce(expectedResponse);
+    });
+
     test('should return 200 status', done => {
       request(app).get('/hello').expect(200, done);
     });
 
     test('should return "Hello, World!"', done => {
-      request(app).get('/hello').expect('Hello, World!', done);
+      request(app).get('/hello').expect(expectedResponse, done);
     });
   });
 
   describe('Given /hello/Johnny', () => {
     const name = 'Johnny';
 
+    beforeEach(() => {
+      mockGreeting.mockImplementation(name => name);
+    });
+
     test('should return 200 status', done => {
       request(app).get(`/hello/${name}`).expect(200, done);
     });
 
     test('should return "Hello, Johnny!"', done => {
-      request(app).get(`/hello/${name}`).expect(`Hello, ${name}!`, done);
+      request(app).get(`/hello/${name}`).expect(name, done);
     });
   })
 
