8c70128d9bad975b2a14a4305375fd4f3e6be637
Sean Sundberg <seansund@us.ibm.com>
2019-04-24 11:38:57 -0500

    Applies logging framework to hello-world

--- src/controllers/hello-world.controller.ts
+++ src/controllers/hello-world.controller.ts
@@ -1,20 +1,32 @@
 import {GET, Path, PathParam} from 'typescript-rest';
 import {Inject} from 'typescript-ioc';
 import {HelloWorldApi} from '../services';
+import {ConsoleLoggerService, LoggerApi} from '../logger';
 
 @Path('/hello')
 export class HelloWorldController {
-  @Inject
-  service: HelloWorldApi;
+
+  logger: LoggerApi;
+
+  constructor(
+    @Inject
+    private service: HelloWorldApi,
+    @Inject
+    logger: LoggerApi = new ConsoleLoggerService(),
+  ) {
+    this.logger = logger.child('HelloWorldController');
+  }
 
   @GET
   async sayHelloToUnknownUser(): Promise<string> {
+    this.logger.info('Saying hello to someone');
     return this.service.greeting();
   }
 
   @Path(':name')
   @GET
   async sayHello(@PathParam('name') name: string): Promise<string> {
+    this.logger.info(`Saying hello to ${name}`);
     return this.service.greeting(name);
   }
 }
--- src/services/hello-world.service.ts
+++ src/services/hello-world.service.ts
@@ -1,9 +1,20 @@
 import {HelloWorldApi} from './hello-world.api';
-import {Provides} from 'typescript-ioc';
+import {Inject, Provides} from 'typescript-ioc';
+import {ConsoleLoggerService, LoggerApi} from '../logger';
 
 @Provides(HelloWorldApi)
 export class HelloWorldService implements HelloWorldApi {
+  logger: LoggerApi;
+
+  constructor(
+    @Inject
+    logger: LoggerApi = new ConsoleLoggerService(),
+  ) {
+    this.logger = logger.child('HelloWorldService');
+  }
+
   greeting(name: string = 'World'): string {
+    this.logger.info(`Generating greeting for ${name}`);
     return `Hello, ${name}!`;
   }
 }
