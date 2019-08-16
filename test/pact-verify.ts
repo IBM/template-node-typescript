11dc370c4159ae52b0bba7337c87dd465f45a325
Sean Sundberg <seansund@us.ibm.com>
2019-08-16 00:07:31 -0500

    Adds logic to pactVerify to skip server start if no pacts are available. Adds publishVerificationResult=true to pact verify

--- test/pact-verify.ts
+++ test/pact-verify.ts
@@ -27,7 +27,10 @@ async function buildOptions(): Promise<VerifierOptions> {
       pactBrokerUrl
         ? {pactBrokerUrl}
         : {pactUrls: await listPactFiles(path.join(process.cwd(), 'pacts'))},
-      {provider: config.name}
+      {
+          provider: config.name,
+          publishVerificationResult: true,
+      },
     );
 
     console.log('Pact verification options', options);
@@ -43,17 +46,31 @@ async function listPactFiles(pactDir: string): Promise<string[]> {
                 return;
             }
 
+            if (!items || items.length == 0) {
+                reject(new Error('no pact files found'));
+                return;
+            }
+
             resolve(items.map(item => path.join(pactDir, item)));
         });
     });
 }
 
 async function verifyPact() {
+    const options = await buildOptions().catch(err => {
+        console.log('Error building pact options: ' + err.message);
+        return null;
+    });
+
+    if (!options) {
+        return;
+    }
+
     console.log('Starting server');
     const server: ApiServer = await buildApiServer().start();
 
     try {
-        await new Verifier().verifyProvider(await buildOptions());
+        await new Verifier().verifyProvider(options);
     } finally {
         await server.stop();
     }
